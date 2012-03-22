e315d9b002770b11f9d1ecfa0ac3c28f803cb65b
John Griffin <johnog@gmail.com>
2012-03-22 14:23:51 +0000

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pump::Application.config.secret_token = '02cb27ede89b13a08d2e7a6a62dfa39bc090649457c28804b3ca87815b5ae05f0c0692dfc63e7e61c620a1ffafcdfcb5783fcbda9f0cb2cdd50a3ce75f14b884'
